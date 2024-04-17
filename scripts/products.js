b5ecaead79d4a480c5fe564a285d67dd6caea334
Angelo Carlotto <angelocarlotto@gmail.com>
2024-04-17 19:02:43 -0400

    dding comments

--- scripts/products.js
+++ scripts/products.js
@@ -1,3 +1,4 @@
+//here we recover the element from HTML file 
 const prev = document.getElementById("prev-btn");
 const next = document.getElementById("next-btn");
 const list = document.getElementById("item-list");
@@ -5,10 +6,12 @@ const list = document.getElementById("item-list");
 const itemWidth = 150;
 const padding = 10;
 
+//add the event listener on the click on the prev button
 prev.addEventListener("click", () => {
   list.scrollLeft -= itemWidth + padding;
 });
 
+//add the event listener on the click on the next button
 next.addEventListener("click", () => {
   list.scrollLeft += itemWidth + padding;
 });
